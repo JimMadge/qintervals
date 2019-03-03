15b30b5344a7b4e8dd71952887bd4094f04e5ba0
Jim Madge <jmmadge@gmail.com>
2019-03-03 12:50:42 +0000

    Tidy gui.py

--- qintervals/gui.py
+++ qintervals/gui.py
@@ -18,10 +18,10 @@
 #  MA 02110-1301, USA.
 #
 #
-from .workout import Workout, WorkoutState
+from .workout import WorkoutState
 from PyQt5 import QtCore, QtGui, QtWidgets, QtMultimedia
 from os import path
-from math import hypot, sin, pi
+from math import sin, pi
 import argparse
 
 _WIDTH = 700
@@ -29,9 +29,10 @@ _HEIGHT = 500
 _BORDER_HORIZONTAL = 25
 _BORDER_VERTICAL = 25
 
-_COLOUR_RED = QtGui.QColor(228,26,28)
-_COLOUR_BLUE = QtGui.QColor(55,126,184)
-_COLOUR_GREEN = QtGui.QColor(77,175,74)
+_COLOUR_RED = QtGui.QColor(228, 26, 28)
+_COLOUR_BLUE = QtGui.QColor(55, 126, 184)
+_COLOUR_GREEN = QtGui.QColor(77, 175, 74)
+
 
 class Ui(QtWidgets.QWidget):
     def __init__(self, workout):
@@ -66,7 +67,7 @@ class Ui(QtWidgets.QWidget):
         self.font_upcoming_header = self.font_interval_name
 
     # Initialise the ui
-    def init_ui(self,workout):
+    def init_ui(self, workout):
         self.workout = workout
 
         # Size the widget
@@ -76,8 +77,10 @@ class Ui(QtWidgets.QWidget):
 
         # Main grid layout
         self.grid_widget = QtWidgets.QWidget(self)
-        self.grid_widget.setGeometry(QtCore.QRect(_BORDER_HORIZONTAL, _BORDER_VERTICAL,
-            _WIDTH-2*_BORDER_HORIZONTAL, _HEIGHT-2*_BORDER_VERTICAL))
+        self.grid_widget.setGeometry(
+            QtCore.QRect(_BORDER_HORIZONTAL, _BORDER_VERTICAL,
+                         _WIDTH-2*_BORDER_HORIZONTAL,
+                         _HEIGHT-2*_BORDER_VERTICAL))
         self.grid_layout = QtWidgets.QGridLayout(self.grid_widget)
         self.grid_layout.setContentsMargins(0, 0, 0, 0)
 
@@ -85,7 +88,8 @@ class Ui(QtWidgets.QWidget):
         self.label_workout_name = QtWidgets.QLabel(self.grid_widget)
         self.label_workout_name.setFont(self.font_workout_name)
         self.label_workout_name.setText(self.workout.name)
-        self.grid_layout.addWidget(self.label_workout_name, 0, 0, 1, -1, QtCore.Qt.AlignCenter)
+        self.grid_layout.addWidget(self.label_workout_name, 0, 0, 1, -1,
+                                   QtCore.Qt.AlignCenter)
 
         # Interval name
         self.label_interval_name = QtWidgets.QLabel(self)
@@ -98,24 +102,30 @@ class Ui(QtWidgets.QWidget):
 
         # Count down
         self.count_down = CountDown(self)
-        self.count_down.addWidget(self.label_interval_name, QtCore.Qt.AlignCenter)
+        self.count_down.addWidget(self.label_interval_name,
+                                  QtCore.Qt.AlignCenter)
         self.count_down.addWidget(self.timers, QtCore.Qt.AlignCenter)
-        self.grid_layout.addWidget(self.count_down, 1, 0, 1, 1, QtCore.Qt.AlignCenter)
+        self.grid_layout.addWidget(self.count_down, 1, 0, 1, 1,
+                                   QtCore.Qt.AlignCenter)
 
         # Upcoming intervals widget
         self.upcoming_intervals = UpcomingIntervals(self)
-        self.grid_layout.addWidget(self.upcoming_intervals, 1, 1, 2, 1, QtCore.Qt.AlignCenter)
+        self.grid_layout.addWidget(self.upcoming_intervals, 1, 1, 2, 1,
+                                   QtCore.Qt.AlignCenter)
 
         # Sound for changing interval
         self.bell = QtMultimedia.QSound(path.dirname(__file__)+'/tone.wav')
 
         # Create buttons
         self.buttons = Buttons(self)
-        self.grid_layout.addWidget(self.buttons, 2, 0, 1, 1, QtCore.Qt.AlignCenter)
+        self.grid_layout.addWidget(self.buttons, 2, 0, 1, 1,
+                                   QtCore.Qt.AlignCenter)
         # Create start/pause shortcut
-        self.shortcut_start_pause = QtWidgets.QShortcut(QtCore.Qt.Key_Space, self, self.start_pause)
+        self.shortcut_start_pause = QtWidgets.QShortcut(
+            QtCore.Qt.Key_Space, self, self.start_pause)
         # Create stop shortcut
-        self.shortcut_stop = QtWidgets.QShortcut(QtCore.Qt.Key_S, self, self.stop)
+        self.shortcut_stop = QtWidgets.QShortcut(QtCore.Qt.Key_S, self,
+                                                 self.stop)
 
         # Initialise timer
         self.timer = QtCore.QTimer()
@@ -128,11 +138,13 @@ class Ui(QtWidgets.QWidget):
     # Redraw dynamic elements of the ui
     def redraw(self):
         # Obtain current time elapsed and remaining, and current interval
-        elapsed, remaining, interval_elapsed, interval_remaining, interval, changed_interval = self.workout.progress()
+        (elapsed, remaining, interval_elapsed, interval_remaining, interval,
+         changed_interval) = self.workout.progress()
 
         self.timers.update_times(remaining, interval_remaining)
 
-        self.count_down.update_times(elapsed,remaining,interval_elapsed,interval_remaining)
+        self.count_down.update_times(elapsed, remaining, interval_elapsed,
+                                     interval_remaining)
         self.count_down.repaint()
 
         self.buttons.update_buttons()
@@ -157,8 +169,9 @@ class Ui(QtWidgets.QWidget):
         self.upcoming_intervals.write_upcoming_intervals()
         self.buttons.update_buttons()
 
+
 class Timers(QtWidgets.QWidget):
-    def __init__(self,parent):
+    def __init__(self, parent):
         super().__init__(parent)
 
         font_time = self.parentWidget().font_time
@@ -169,37 +182,43 @@ class Timers(QtWidgets.QWidget):
         # Interval remaining label
         self.label_interval_remaining = QtWidgets.QLabel(self)
         self.label_interval_remaining.setText("Interval")
-        self.grid_layout.addWidget(self.label_interval_remaining, 2, 0, QtCore.Qt.AlignLeft)
+        self.grid_layout.addWidget(self.label_interval_remaining, 2, 0,
+                                   QtCore.Qt.AlignLeft)
         # Interval remaining time
         self.label_interval_remaining_time = QtWidgets.QLabel(self)
         self.label_interval_remaining_time.setFont(font_time)
         self.label_interval_remaining_time.setText(self.time_str(0))
-        self.grid_layout.addWidget(self.label_interval_remaining_time, 2, 1, QtCore.Qt.AlignRight)
+        self.grid_layout.addWidget(self.label_interval_remaining_time, 2, 1,
+                                   QtCore.Qt.AlignRight)
 
         # Total remaining label
         self.label_total_remaining = QtWidgets.QLabel(self)
         self.label_total_remaining.setText("Total")
-        self.grid_layout.addWidget(self.label_total_remaining, 3, 0, QtCore.Qt.AlignLeft)
+        self.grid_layout.addWidget(self.label_total_remaining, 3, 0,
+                                   QtCore.Qt.AlignLeft)
         # Total remaining time
         self.label_total_remaining_time = QtWidgets.QLabel(self)
         self.label_total_remaining_time.setFont(font_time)
         self.label_total_remaining_time.setText(self.time_str(0))
-        self.grid_layout.addWidget(self.label_total_remaining_time, 3, 1, QtCore.Qt.AlignRight)
+        self.grid_layout.addWidget(self.label_total_remaining_time, 3, 1,
+                                   QtCore.Qt.AlignRight)
 
     # Update times
-    def update_times(self,remaining,interval_remaining):
+    def update_times(self, remaining, interval_remaining):
         # Write current times
-        self.label_interval_remaining_time.setText(self.time_str(interval_remaining))
+        self.label_interval_remaining_time.setText(
+            self.time_str(interval_remaining))
         self.label_total_remaining_time.setText(self.time_str(remaining))
 
     # Format a time in seconds for output
-    def time_str(self,time):
+    def time_str(self, time):
         minutes, seconds = divmod(time, 60)
         string = "{:2d}:{:04.1f}".format(int(minutes), seconds)
         return string
 
+
 class Buttons(QtWidgets.QWidget):
-    def __init__(self,parent):
+    def __init__(self, parent):
         super().__init__(parent)
 
         self.workout = self.parentWidget().workout
@@ -211,7 +230,8 @@ class Buttons(QtWidgets.QWidget):
         # Create start/pause button
         self.button_start_pause = QtWidgets.QPushButton('Start', self)
         self.button_start_pause.setFont(font_default)
-        self.button_start_pause.clicked.connect(self.parentWidget().start_pause)
+        self.button_start_pause.clicked.connect(
+            self.parentWidget().start_pause)
         self.hbox.addWidget(self.button_start_pause, QtCore.Qt.AlignCenter)
 
         # Create stop button
@@ -232,8 +252,9 @@ class Buttons(QtWidgets.QWidget):
             self.button_start_pause.setText('Start')
             self.button_stop.setEnabled(False)
 
+
 class CountDown(QtWidgets.QWidget):
-    def __init__(self,parent):
+    def __init__(self, parent):
         super().__init__(parent)
 
         # Dimension of the sides of this widget
@@ -242,13 +263,15 @@ class CountDown(QtWidgets.QWidget):
         # Angle of a full circle in Qt (360*16)
         self._TOTAL_ANGLE = 5760
 
-        self.setMinimumSize(self._DIMENSION,self._DIMENSION)
+        self.setMinimumSize(self._DIMENSION, self._DIMENSION)
 
         # Pens used to draw arcs
         self._PEN_WIDTH = 10
         self._HALF_PEN_WIDTH = self._PEN_WIDTH/2
-        self.pen_total = QtGui.QPen(_COLOUR_BLUE, self._PEN_WIDTH, QtCore.Qt.SolidLine)
-        self.pen_interval = QtGui.QPen(_COLOUR_GREEN, self._PEN_WIDTH, QtCore.Qt.SolidLine)
+        self.pen_total = QtGui.QPen(_COLOUR_BLUE, self._PEN_WIDTH,
+                                    QtCore.Qt.SolidLine)
+        self.pen_interval = QtGui.QPen(_COLOUR_GREEN, self._PEN_WIDTH,
+                                       QtCore.Qt.SolidLine)
 
         # V box in the centre of the arcs
         self._CENTRE = self._DIMENSION/2
@@ -257,15 +280,20 @@ class CountDown(QtWidgets.QWidget):
         self.contents = QtWidgets.QWidget(self)
         self.contents.setGeometry(QtCore.QRect(
             QtCore.QPoint(self._INSET, self._INSET),
-            QtCore.QPoint(self._DIMENSION-self._INSET, self._DIMENSION-self._INSET)))
+            QtCore.QPoint(self._DIMENSION-self._INSET,
+                          self._DIMENSION-self._INSET)))
         self.vbox = QtWidgets.QVBoxLayout(self.contents)
         self.vbox.setContentsMargins(0, 0, 0, 0)
 
-        self.update_times(0,1,0,1)
+        self.update_times(0, 1, 0, 1)
 
-    def update_times(self, elapsed, remaining, interval_elapsed, interval_remaining):
-        self.angle_remaining = remaining / (elapsed+remaining) * self._TOTAL_ANGLE
-        self.angle_interval_remaining = interval_remaining / (interval_elapsed+interval_remaining) * self._TOTAL_ANGLE
+    def update_times(self, elapsed, remaining, interval_elapsed,
+                     interval_remaining):
+        self.angle_remaining = (remaining / (elapsed+remaining)
+                                * self._TOTAL_ANGLE)
+        self.angle_interval_remaining = (
+            interval_remaining / (interval_elapsed+interval_remaining)
+            * self._TOTAL_ANGLE)
 
     def addWidget(self, *args):
         self.vbox.addWidget(*args)
@@ -280,21 +308,24 @@ class CountDown(QtWidgets.QWidget):
         # Total time arc
         painter.setPen(self.pen_total)
         painter.drawArc(self._HALF_PEN_WIDTH,
-                self._HALF_PEN_WIDTH,
-                width-self._PEN_WIDTH,
-                height-self._PEN_WIDTH,0,self.angle_remaining)
+                        self._HALF_PEN_WIDTH,
+                        width-self._PEN_WIDTH,
+                        height-self._PEN_WIDTH,
+                        0, self.angle_remaining)
 
         # Interval time arc
         painter.setPen(self.pen_interval)
         painter.drawArc(self._PEN_WIDTH+self._HALF_PEN_WIDTH,
-                self._PEN_WIDTH+self._HALF_PEN_WIDTH,
-                width-3*self._PEN_WIDTH,
-                height-3*self._PEN_WIDTH,0,self.angle_interval_remaining)
+                        self._PEN_WIDTH+self._HALF_PEN_WIDTH,
+                        width-3*self._PEN_WIDTH,
+                        height-3*self._PEN_WIDTH,
+                        0, self.angle_interval_remaining)
 
         painter.end()
 
+
 class UpcomingIntervals(QtWidgets.QWidget):
-    def __init__(self,parent):
+    def __init__(self, parent):
         super().__init__(parent)
 
         self._UPCOMING_INTERVALS_DISPLAYED = 8
@@ -308,10 +339,13 @@ class UpcomingIntervals(QtWidgets.QWidget):
         self.label_upcoming = QtWidgets.QLabel(self)
         self.label_upcoming.setFont(font_upcoming_header)
         self.label_upcoming.setText("Upcoming")
-        self.vbox_upcoming.addWidget(self.label_upcoming, QtCore.Qt.AlignCenter)
+        self.vbox_upcoming.addWidget(self.label_upcoming,
+                                     QtCore.Qt.AlignCenter)
 
         # Upcoming interval labels
-        self.label_upcoming_intervals = [QtWidgets.QLabel() for i in range(self._UPCOMING_INTERVALS_DISPLAYED)]
+        self.label_upcoming_intervals = [
+            QtWidgets.QLabel()
+            for i in range(self._UPCOMING_INTERVALS_DISPLAYED)]
         for label in self.label_upcoming_intervals:
             self.vbox_upcoming.addWidget(label, QtCore.Qt.AlignCenter)
         self.write_upcoming_intervals()
@@ -323,16 +357,18 @@ class UpcomingIntervals(QtWidgets.QWidget):
 
         # Set labels of upcoming intevals up to the display limit
         for label, interval in zip(self.label_upcoming_intervals, upcoming):
-                label.setText(interval.text)
+            label.setText(interval.text)
         # Blank the remainder of the labels if the number displayed is greater
         # than the number of intervals remaining
         for label in self.label_upcoming_intervals[len(upcoming):]:
             label.setText('')
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(prog='qintervals', description='Interval training timer')
+    parser = argparse.ArgumentParser(prog='qintervals',
+                                     description='Interval training timer')
     parser.add_argument('workout', type=str, action='store',
-            help='YAML workout file to read')
+                        help='YAML workout file to read')
 
     clargs = parser.parse_args()
     return clargs
